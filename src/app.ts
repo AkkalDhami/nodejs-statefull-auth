ef98c59690b3c07549a34a7e2e192be2ad4c0f8a
AkkalDhami <dhamiakkal21@gmail.com>
2025-12-23 07:40:28 +0545

    Add Google OAuth env vars and app entry point
    Added Google OAuth-related environment variables to .env.example and created the initial src/app.ts file. Also installed the google-auth-library dependency to support Google authentication.

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express, { type Application } from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import swaggerUI from "swagger-ui-express";
+
+import { errorHandler } from "#middlewares/error-handler.js";
+import { ApiResponse } from "#utils/api-response.js";
+// import "./cron-jobs/otp-cleaner";
+import { swaggerSpec } from "./docs/swagger";
+
+import Routes from "#routes/index.js";
+import { googleAuthCallbackHandler } from "#controllers/auth.controller.js";
+
+const app: Application = express();
+
+//*  Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(morgan(process.env.NODE_ENV === "development" ? "dev" : "combined"));
+
+//*  Routes
+app.use("/api", Routes);
+app.get("/auth/google/callback", googleAuthCallbackHandler);
+
+app.get("/", (req, res) => {
+  return ApiResponse.Ok(res, "Welcome to the API");
+});
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+app.use(errorHandler);
+
+export default app;
